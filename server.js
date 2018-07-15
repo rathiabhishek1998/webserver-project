c51f3ab1be9f84054b9a05f1fb6dc79ea0e39a5c
Abhishek Rathi <2016.abhishek.rathi@ves.ac.in>
2018-07-15 20:54:45 +0530

    Added Project page

--- server.js
+++ server.js
@@ -50,6 +50,13 @@ app.get('/about',(req,res) =>{
     });
 });
 
+app.get('/project',(req,res) =>{
+    res.render('project.hbs',{
+        pageTitle:'Projects',
+    });
+});
+
+
 app.get('/bad',(req,res) =>{
     res.send('ErrorMesssage');
 });
